e799f2d5c75d168c733a98742018c99227d0d544
Rita Bradley <rita@ritabradley.dev>
2023-06-24 12:32:16 -0700

    map through list items and render each item

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,9 +1,12 @@
 import Item from './Item';
 
-export default function PackingList() {
+export default function PackingList({ items }) {
   return (
-    <section className='bg-navy text-whip flex flex-col items-center list justify-between gap-8 py-10'>
+    <section className='bg-navy text-whip list flex flex-col items-center justify-between gap-8 py-10'>
       <ul className='grid content-start justify-center w-4/5 gap-3 overflow-scroll list-none'>
+        {items.map((item) => {
+          return <Item key={item.id} description={item.description} quantity={item.quantity} packed={item.packed} />;
+        })}
         <Item />
       </ul>
     </section>
