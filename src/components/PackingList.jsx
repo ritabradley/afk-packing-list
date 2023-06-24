d33e8c1b100d4cc5593c176d9752ae459c5676a0
Rita Bradley <rita@ritabradley.dev>
2023-06-24 11:50:30 -0700

    style list `section` and `ul`

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -2,8 +2,8 @@ import Item from './Item';
 
 export default function PackingList() {
   return (
-    <section>
-      <ul>
+    <section className='bg-navy text-whip flex flex-col items-center list justify-between gap-8 py-10'>
+      <ul className='grid content-start justify-center w-4/5 gap-3 overflow-scroll list-none'>
         <Item />
       </ul>
     </section>
