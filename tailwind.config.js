2cedf657e24ce1450a5206c412f4921aef22203c
Rita Bradley <rita@ritabradley.dev>
2023-06-23 22:16:23 -0700

    add Google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        whip: '#fdf0d5',
+        maroon: '#780000',
+        lava: '#c1121f',
+        navy: '#003049',
+        cerulean: '#669bbc',
+      },
+      fontFamily: {
+        body: ['"Karla"', 'sans-serif'],
+        heading: ['"Caprasimo"', 'cursive'],
+      },
+    },
   },
   plugins: [],
 };
