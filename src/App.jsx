be70c07ec5c7001d2afb4fb93b60e7ecc198c280
Rita Bradley <rita@ritabradley.dev>
2023-06-24 12:30:01 -0700

    create form submission function
    - get value from select with id of `quantity`
    - get value from input with id of `description`
    - create a new item object with the values from the form
    - add the new item to the items array
    - clear the form

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,34 @@ import Stats from './components/Stats';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function onFormSubmit(e) {
+    e.preventDefault();
+    // get value from select with id of `quantity`
+    const quantity = document.querySelector(`#quantity`).value;
+    // get value from input with id of `description`
+    const description = document.querySelector(`#description`).value;
+    // create a new item object with the values from the form
+    const newItem = {
+      id: items.length + 1,
+      quantity,
+      description,
+      packed: false,
+    };
+    // add the new item to the items array
+    setItems([...items, newItem]);
+    // clear the form
+    document.querySelector(`#quantity`).value = ``;
+    document.querySelector(`#description`).value = ``;
+    console.log(`submitted`);
+  }
+
   return (
     <div className='app font-body text-navy grid w-full h-screen'>
       <Header />
-      <Form />
-      <PackingList />
+      <Form onSubmit={onFormSubmit} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
