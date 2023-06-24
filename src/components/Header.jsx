b1e860c9b4274376c35c307c3c07344ef29ec1c9
Rita Bradley <rita@ritabradley.dev>
2023-06-23 22:36:36 -0700

    finish styling header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,7 @@
 export default function Header() {
-  return <h1 className='font-heading py-6 text-6xl tracking-tight text-center uppercase'>AFK Packing List</h1>;
+  return (
+    <h1 className='font-heading bg-cerulean py-6 text-6xl tracking-tight text-center uppercase'>
+      ✈️ AFK Packing List ⌨️
+    </h1>
+  );
 }
