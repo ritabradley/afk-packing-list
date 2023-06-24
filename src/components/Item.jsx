65759eb7e53021cdad68a83d6f974e84307bc20c
Rita Bradley <rita@ritabradley.dev>
2023-06-24 11:48:57 -0700

    move checkbox inside list item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,8 @@
 export default function Item() {
   return (
     <>
-      <input className='accent-cerulean w-5 h-5' type='checkbox' name='' id='' />
       <li className='flex items-center gap-3'>
+        <input className='accent-cerulean w-5 h-5' type='checkbox' name='' id='' />
         item <button className='bg-none p-2 text-lg translate-y-0.5 border-none cursor-pointer'>❌</button>
       </li>
     </>
