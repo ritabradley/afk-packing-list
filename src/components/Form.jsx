3cc52fb32fcd4ef12bfeab11ee928ac1050632ff
Rita Bradley <rita@ritabradley.dev>
2023-06-23 22:44:11 -0700

    give form background color and spacing

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,7 @@
 export default function Form() {
   return (
-    <form>
+    <form className='bg-lava py-7 flex items-center justify-center gap-3'>
+      <h3 className='mr-4 text-2xl'>what do you need for your trip?</h3>
       <input type='select' />
     </form>
   );
