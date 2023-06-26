5abfd05b593bd954e5f4a8d57ec7c59758f004d6
Rita Bradley <rita@ritabradley.dev>
2023-06-25 22:05:30 -0700

    set `select` default value to 1

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,10 +2,7 @@ export default function Form({ onSubmit }) {
   return (
     <form className='bg-lava py-7 flex items-center justify-center gap-3' onSubmit={onSubmit}>
       <h3 className='mr-4 text-2xl'>what do you need for your trip?</h3>
-      <select defaultValue={'DEFAULT'} id='quantity' className='pill focus:outline-lava'>
-        <option value='DEFAULT' disabled>
-          Quantity
-        </option>
+      <select defaultValue={1} id='quantity' className='pill focus:outline-lava'>
         {/* 
           1. First, we create an array with 20 empty items by using the Array constructor and the ES6 array spread operator. 
           2. Then, we call the map() method to iterate over the array and create a new array with the number of options we need.
