011a6c2fddc1caa655531e4a0f01299c69915f0c
Rita Bradley <rita@ritabradley.dev>
2023-06-25 21:15:40 -0700

    add span for item quantity and description
    - add conditional style for if item is packed

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,9 +1,12 @@
-export default function Item() {
+export default function Item({ description, quantity, packed }) {
   return (
     <>
       <li className='flex items-center gap-3'>
         <input className='accent-cerulean w-5 h-5' type='checkbox' name='' id='' />
-        item <button className='bg-none p-2 text-lg translate-y-0.5 border-none cursor-pointer'>❌</button>
+        <span style={packed ? { textDecoration: 'strikethrough' } : {}}>
+          {quantity} {description}
+        </span>
+        <button className='bg-none p-2 text-lg translate-y-0.5 border-none cursor-pointer'>❌</button>
       </li>
     </>
   );
