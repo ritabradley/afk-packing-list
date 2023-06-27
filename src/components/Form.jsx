97f2e76576d2a29a2dcd604f8b339d77a9f97028
Rita Bradley <rita@ritabradley.dev>
2023-06-26 18:36:08 -0700

    put `select` and `input` in a div for styling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,8 +2,9 @@ export default function Form({ onFormSubmit, quantity, description, onQuantityCh
   return (
     <form className='bg-lava py-7 flex items-center justify-center gap-3' onSubmit={onFormSubmit}>
       <h3 className='mr-4 text-2xl'>what do you need for your trip?</h3>
-      <select value={quantity} onChange={onQuantityChange} id='quantity' className='pill focus:outline-lava'>
-        {/* 
+      <div className='flex gap-3'>
+        <select value={quantity} onChange={onQuantityChange} id='quantity' className='pill focus:outline-lava'>
+          {/* 
           1. First, we create an array with 20 empty items by using the Array constructor and the ES6 array spread operator. 
           2. Then, we call the map() method to iterate over the array and create a new array with the number of options we need.
           3. The map() method calls the callback function on each array item. 
@@ -11,20 +12,22 @@ export default function Form({ onFormSubmit, quantity, description, onQuantityCh
           5. The value of the second argument is always the index of the current item. 
           6. We use the index to generate a unique key for each option. 
           */}
-        {[...Array(20)].map((_, i) => (
-          <option key={i} value={i + 1}>
-            {i + 1}
-          </option>
-        ))}
-      </select>
-      <input
-        value={description}
-        onChange={onDescriptionChange}
-        id='description'
-        className='pill focus:outline-lava'
-        type='text'
-        placeholder='Item...'
-      />
+          {[...Array(20)].map((_, i) => (
+            <option key={i} value={i + 1}>
+              {i + 1}
+            </option>
+          ))}
+        </select>
+        <input
+          value={description}
+          onChange={onDescriptionChange}
+          id='description'
+          className='pill focus:outline-lava'
+          type='text'
+          placeholder='Item...'
+        />
+      </div>
+
       <button className='pill bg-cerulean focus:outline-navy uppercase' type='submit'>
         Add
       </button>
